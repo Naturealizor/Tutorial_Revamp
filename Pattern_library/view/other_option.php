ba9afb747e5f0b3f90f03ee9abd1433869ab5012
Naturealizor <31334963+Naturealizor@users.noreply.github.com>
2019-05-11 20:45:56 -0500

    started tag library, setup scss and css compile, started css

--- Pattern_library/view/other_option.php
+++ Pattern_library/view/other_option.php
@@ -2,7 +2,18 @@
 include('C:\Users\Jacob\Desktop\School\Spring_2019\C++\Week_4\Tutorial_Revamp\Pattern_library\view\header.php');
 ?>
 
+<body>
+	<main>
+		<h1>HTML tags and what they do</h1>
+		<p>
+			
+		</p>
+	</main>
+</body>
 
+<?php
+include('C:\Users\natur\Documents\GitHub\Tutorial_Revamp\Pattern_library\view\footer.php');
+?>
 <!-- Make the tutorial display the html tags and have snipets of them to make it simpler
 	Go from <doctype! html> all the way down a file
 -->
