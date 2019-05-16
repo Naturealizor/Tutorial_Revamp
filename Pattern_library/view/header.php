4ee5e6b8ca784f04c6548485a2cbc7511fd49f76
Naturealizor <31334963+Naturealizor@users.noreply.github.com>
2019-05-15 19:21:40 -0500

    switched to php, made template page, added to base.scss

--- Pattern_library/view/header.php
+++ Pattern_library/view/header.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
 
+
     	<!-- Global site tag (gtag.js) - Google Analytics -->
         <!-- Do we Need this??????????-->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-19389139-1"></script>
@@ -11,29 +12,28 @@
         gtag('js', new Date());
         gtag('config', 'UA-19389139-1');
     </script>
-
+    <title>HTML Elements</title>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="Butlercc Webhosting">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <link rel="shortcut icon" href="/favicon.ico">
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png">  
-    <!-- Figure out if I want to keep the font-awesome stuff -->
-    <link rel="stylesheet" href="/font-awesome-4.2.0/css/font-awesome.css"> 
+    <link rel="stylesheet" href="../css/styles.css">
+            <!-- Figure out if I want to keep the font-awesome stuff -->
+    <!-- <link rel="stylesheet" href="/font-awesome-4.2.0/css/font-awesome.css">  -->
     
-</head>  
+</head>
     <body>
-
-	<header> 
-        <nav>
-            <ul>
-                <li><a href="home.php">Home</a></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
-        </nav>
-	</header>
-
-
+        <header>
+                <div id="branding">
+                        <h1>ButlerCC Webhosting</h1>
+                        <p>Free Webhosting for ButlerCC WebDev Students</p>
+                </div>
+        </header>
+        <!-- add CSS -->
+        <div class="sidenav">
+            <a href="">#1</a>
+            <a href="">#2</a>
+            <a href="">#3</a>
+            <a href="">#4</a>
+            <a href="">#5</a>
+        </div>
---
+++ Pattern_library/view/template.php
@@ -0,0 +1,61 @@
+<?php 
+require_once('../util/main.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    	<!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-19389139-1">
+    </script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-19389139-1');
+    </script>
+
+    <title>HTML Template</title>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Butlercc Webhosting">
+    <link rel="stylesheet" href="../css/styles.css">    
+</head>
+    <body>
+        <header>
+                <div id="branding">
+                        <h1>ButlerCC Webhosting</h1>
+                        <p>Free Webhosting for ButlerCC WebDev Students</p>
+                </div>
+        </header>
+        <div class="sidenav">
+            <a href="">#1</a>
+            <a href="">#2</a>
+            <a href="">#3</a>
+            <a href="">#4</a>
+            <a href="">#5</a>
+        </div>
+        <main>
+        <article>
+            <div class="main_content_stack">
+                <div class="main_content_wrapper">
+                    <h1>Page Title</h1>
+                        <p> Content goes here</p>
+                        <p> Content goes here</p>
+
+                        <div class="card_container">
+                        <h3 class="card_header">Card Title</h3>
+                        <p class="card_desc">Card information goes here.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </article>
+        </main>
+    </body>
+    <footer>
+
+    </footer>
+</html>
+
+    
